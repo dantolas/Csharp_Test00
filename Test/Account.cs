649cdb2c3f19d91ce72431c99bc9de1edbc16316
Samuel Kuta <kuta.samuel@gmail.com>
2022-09-26 09:06:28 +0200

    Add files via upload

---
+++ Test/Account.cs
@@ -0,0 +1,93 @@
+﻿namespace Test
+{
+    class Account
+    {
+        private float limit;
+        private float balance;
+        private String currency;
+        private bool frozen = false;
+
+
+        public Account(float limit, float balance, string currency)
+        {
+            while(balance > limit)
+            {
+                Console.WriteLine("Balance cannot be set higher than the limit, please enter the balance again:");
+                float input = (float)Convert.ToInt32(Console.ReadLine());
+                balance = input;
+            }
+
+            this.limit = limit;
+            this.balance = balance;
+            this.currency = currency;
+        }
+
+        public void freeze(int input)
+        {
+            if(input > 0)
+            {
+                this.frozen = true;
+                return;
+            }
+            this.frozen = false;
+        }
+
+
+        public void payDebt()
+        {
+            Console.WriteLine("Debt paid");
+            this.balance = this.limit;
+        }
+
+
+        public float getAsEuros()
+        {
+            this.currency = "eur";
+            this.balance = balance / 25;
+            this.limit = limit / 25;
+            return this.balance;
+        }
+
+        public void raiseLimit(float amount)
+        {
+            if (this.frozen)
+            {
+                Console.WriteLine("Operation failed: Account is frozen");
+                return;
+            }
+            this.limit += amount;
+            return;
+        }
+
+
+        public void makePayment(float amount)
+        {
+
+            if (this.frozen)
+            {
+                Console.WriteLine("Operation failed: Account is frozen");
+                return;
+            }
+
+            if(amount <= this.balance)
+            {
+                this.balance -= amount;
+                Console.WriteLine("Payment succesfull");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Operation failed:Not enough balance on account");
+                return;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "- - - - - - - - - - - \nAccount limit:"+this.limit+this.currency + "\nAccount balance:"+this.balance+this.currency+ "\n- - - - - - - - - - - ";
+        }
+
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user");
+
+            Account a = new Account(500, 600, "kc");
+
+            Console.WriteLine(a);
+
+
+            a.raiseLimit(400);
+
+            a.makePayment(50);
+            a.makePayment(50);
+            a.makePayment(500);
+
+            Console.WriteLine(a);
+
+            Console.WriteLine("Balance in euros:" + a.getAsEuros());
+
+            
+
+            Console.WriteLine(a);
+
+
+            a.freeze(1);
+
+            a.makePayment(20);
+            a.raiseLimit(30);
+            Console.WriteLine(a);
+            a.freeze(0);
+            a.makePayment(2);
+            a.raiseLimit(5);
+            Console.WriteLine(a);
+
+        }
+    }
+}
